54aca677635e3979b97145bacb2898f0e700a52d
Santiago Lamora <santiago@ribaguifi.com>
2023-03-08 14:06:54 +0100

    Update counters & add real testimonials

--- canalroya/views.py
+++ canalroya/views.py
@@ -1,14 +1,24 @@
+from typing import Any, Dict
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
 from canalroya.models import Testimonial
 
 
-class TestimonialCreateView(CreateView):
+class CanalRoyaContextMixin:
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "testimonial_count": Testimonial.objects.count(),
+        })
+        return context
+
+
+class TestimonialCreateView(CanalRoyaContextMixin, CreateView):
     model = Testimonial
     fields = ("first_name", "last_name", "profession", "comment", "image", "email")
     success_url = reverse_lazy('canalroya:testimonial-thanks')
 
 
-class TestimonialThanksView(TemplateView):
+class TestimonialThanksView(CanalRoyaContextMixin, TemplateView):
     template_name = "canalroya/testimonial_thanks.html"
