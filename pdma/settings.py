5a9d3bdcb55cc682f4c082132fdadeff5d3c4aaf
Santiago Lamora <santiago@ribaguifi.com>
2023-03-08 23:31:27 +0100

    Decouple STATIC_ROOT & MEDIA_ROOT

--- pdma/settings.py
+++ pdma/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = env('STATIC_ROOT')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = env('MEDIA_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
