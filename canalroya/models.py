6bcd23ddf2775f6ee1c6a794fbd9a01a88943700
Santiago Lamora <santiago@ribaguifi.com>
2023-03-11 00:09:40 +0100

    Update max length to 150 on profession field

--- canalroya/models.py
+++ canalroya/models.py
@@ -24,7 +24,7 @@ class Testimonial(models.Model):
 
     first_name = models.CharField('Nombre', max_length=30)
     last_name = models.CharField('Apellidos', max_length=150)
-    profession = models.CharField('Profesión', max_length=50)
+    profession = models.CharField('Profesión', max_length=150)
     city = models.CharField('Localidad', max_length=50)
     province = models.CharField('Provincia', max_length=50, choices=get_province_choices())
     comment = models.TextField('Comentarios')
