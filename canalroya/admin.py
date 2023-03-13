311dc542708df627a421858fc84618bfe55c3db8
Santiago Lamora <santiago@ribaguifi.com>
2023-03-13 16:46:19 +0100

    Configure django.admin to manage Testimonials

--- canalroya/admin.py
+++ canalroya/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from canalroya.models import Testimonial
 
-# Register your models here.
+
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ("get_full_name", "priority", "status", "created_at", "city", "province", "comment")
+    list_filter = ("status", "province")
+
+
+admin.site.register(Testimonial, TestimonialAdmin)
--- pdma/urls.py
+++ pdma/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
+    path('peacemaker/', admin.site.urls),
     path('', include('canalroya.urls')),
 ]
 
