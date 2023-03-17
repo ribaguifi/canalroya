436071f7633ed60a0c863478e4e2c83742474fc2
Santiago Lamora <santiago@ribaguifi.com>
2023-03-17 12:58:40 +0100

    Set legal links on footer

--- canalroya/urls.py
+++ canalroya/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     path('unirme/', views.TestimonialCreateView.as_view(), name='testimonial-new'),
     path('gracias/', views.TestimonialThanksView.as_view(), name='testimonial-thanks'),
     path('la-voz-de-la-montana/', views.TestimonialListView.as_view(), name='testimonial-list'),
-    # TODO(@slamora): set proper URL
+
+    path('aviso-legal/', RedirectView.as_view(
+        url='https://elpirineonosevende.org/aviso-legal/'), name='legal-notice'),
     path('politica-de-privacidad/', RedirectView.as_view(
         url='https://elpirineonosevende.org/politica-de-privacidad/'), name='privacy-policy'),
+    path('politica-de-cookies/', RedirectView.as_view(
+        url='https://elpirineonosevende.org/politica-de-cookies/'), name='cookies-policy'),
 ]
