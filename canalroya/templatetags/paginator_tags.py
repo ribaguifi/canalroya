6eb2c6223310dc5b686765e509cbb760306a6aca
Santiago Lamora <santiago@ribaguifi.com>
2023-03-20 23:29:35 +0100

    Use templatetag to build paginator URLs

--- canalroya/templatetags/paginator_tags.py
+++ canalroya/templatetags/paginator_tags.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django import template
 from django.core.paginator import Paginator
 
@@ -10,3 +12,12 @@ def get_proper_elided_page_range(p, number, on_each_side=3, on_ends=2):
     return paginator.get_elided_page_range(number=number,
                                            on_each_side=on_each_side,
                                            on_ends=on_ends)
+
+
+# credits: https://stackoverflow.com/a/36288962
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.pop('page', None)
+    query.update(kwargs)
+    return query.urlencode()
