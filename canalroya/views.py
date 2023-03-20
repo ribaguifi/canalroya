4705541ac58bec252ca3caeb937564ec07815dc2
Santiago Lamora <santiago@ribaguifi.com>
2023-03-21 00:03:07 +0100

    Implement search by full name

--- canalroya/views.py
+++ canalroya/views.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 from django.conf import settings
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, TemplateView
+from django.views.generic import CreateView, ListView, UpdateView, TemplateView
 
 from canalroya.forms import TestimonialForm
 from canalroya.models import Testimonial
@@ -26,13 +26,21 @@ class TestimonialCreateView(CanalRoyaContextMixin, CreateView):
 class TestimonialThanksView(CanalRoyaContextMixin, TemplateView):
     template_name = "canalroya/testimonial_thanks.html"
 
-
+from django.db.models.functions import Concat
+from django.db.models import CharField, Value
 class TestimonialListView(CanalRoyaContextMixin, ListView):
     model = Testimonial
     paginate_by = 33
 
     def get_queryset(self):
         qs = Testimonial.objects.filter(status=Testimonial.Status.APPROVED).order_by("priority", "created_at")
+        q = self.clean_search_query()
+        if q:
+            qs = qs.annotate(
+                fullname=Concat('first_name', Value(" "),
+                                'last_name', output_field=CharField()),
+            ).filter(fullname__icontains=q)
+
         self.region = self.clean_region()
         if self.region:
             if self.region == Testimonial.Region.HUESCA:
@@ -40,6 +48,10 @@ class TestimonialListView(CanalRoyaContextMixin, ListView):
             elif self.region == Testimonial.Region.ARAGON:
                 qs = qs.filter(province__in=[Testimonial.Region.HUESCA.label, Testimonial.Region.ZARAGOZA.label, Testimonial.Region.TERUEL.label])
         return qs
+
+    def clean_search_query(self):
+        q = self.request.GET.get("q", "")
+        return q.strip()
 
     def clean_region(self):
         region = self.request.GET.get("region")
