bc9822b5dfe4e084bd210bb0868f9991858005de
Santiago Lamora <santiago@ribaguifi.com>
2023-03-21 13:00:23 +0100

    Show updated_at field on list

--- canalroya/admin.py
+++ canalroya/admin.py
@@ -9,7 +9,8 @@ from canalroya.models import Testimonial
 
 class TestimonialAdmin(admin.ModelAdmin):
     actions = ['mark_as_approved', 'mark_as_incomplete', 'mark_as_pending', 'mark_as_spam', 'send_to_trash']
-    list_display = ("get_full_name", "priority", "status", "email", "get_created_at", "city", "province", "comment")
+    list_display = ("get_full_name", "priority", "status", "email", "get_created_at",
+                    "get_updated_at", "city", "province", "comment")
     list_filter = ("status", "province")
     search_fields = ['first_name', 'last_name', 'email']
 
@@ -18,6 +19,12 @@ class TestimonialAdmin(admin.ModelAdmin):
         return Template("{{ created_at|date:'SHORT_DATETIME_FORMAT' }}").render(c)
     get_created_at.admin_order_field = "created_at"
     get_created_at.short_description = "Created at"
+
+    def get_updated_at(self, obj):
+        c = Context({"updated_at": obj.updated_at})
+        return Template("{{ updated_at|date:'SHORT_DATETIME_FORMAT' }}").render(c)
+    get_updated_at.admin_order_field = "updated_at"
+    get_updated_at.short_description = "Updated at"
 
     def mark_as_approved(self, request, queryset):
         self.update_status_to(request, queryset, Testimonial.Status.APPROVED)
