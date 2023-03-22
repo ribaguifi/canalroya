aee302de6a18feb2ca20fd94ac7d44e3f8a8ae91
Santiago Lamora <santiago@ribaguifi.com>
2023-03-22 22:42:05 +0100

    Incomplete email

--- canalroya/utils.py
+++ canalroya/utils.py
@@ -12,6 +12,7 @@ Hemos detectado que hay algunos de los siguientes problemas en tu testimonio:
 a) La foto elegida no es personal (tipo autorretrato): p.ej. es un paisaje, un meme o un diseño.
 b) Hay un problema con el formato de la foto (está girada, es demasiado pequeña...)
 c) El lenguaje es político, violento u ofensivo y/o ataca o insulta a una persona o grupo de personas.
+Necesitamos comentarios positivos y constructivos para Salvar Canal Roya. ¿Puedes cambiar el tono del lenguaje?
 
 Por favor, accede a la siguiente dirección para actualizar tu testimonio:
 https://testimonios.elpirineonosevende.org{url}#unirme
