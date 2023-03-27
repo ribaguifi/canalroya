ca7400f95ba57845d195fbd4349ffbe95155ab0e
Santiago Lamora <santiago@ribaguifi.com>
2023-03-27 11:59:37 +0200

    Set LOCALE_PATHS setting to begin i18n

--- pdma/settings.py
+++ pdma/settings.py
@@ -121,6 +121,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [Path(BASE_DIR, 'canalroya/locale')]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
